1689d3641a5bb95172610f1154eea219f3a5b1c3
jyotisg <jyotisg95@gmail.com>
2016-06-12 12:21:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('Hi! I\'m Bot!')
+            return bot.say('Hi! I\'m jyoti Bot!')
                 .then(() => 'askName');
         }
     },
