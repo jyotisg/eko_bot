5eb7b3ce546a4e8439bedca3212164b396d39c75
jyotisg <jyotisg95@gmail.com>
2016-06-13 16:19:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,13 +11,8 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            PythonShell.run('my_script.py', function (err, results) {
-            if (err) throw err;
-            // results is an array consisting of messages collected during execution 
-            console.log('results: %j', results);
              return bot.say(''+results)
                 .then(() => 'askName');
-            });
         }
     },
 
